29454e93ad682f305933acc9e239db65a3d91829
CSC1060-Marsh <143460025+CSC1060-Marsh@users.noreply.github.com>
2023-10-01 15:55:29 -0600

    Added Everything

--- src/Main.java
+++ src/Main.java
@@ -27,7 +27,14 @@ public class Main {
             System.out.println("Your current string is '" + newString + "'.");
             startingString = newString;
         } else if (choice == 2) {
-            //make this do something
+            System.out.println("Substring:");
+            String substring = sc.nextLine();
+            int indexOfSubstring = startingString.indexOf(substring);
+            if (indexOfSubstring == -1) {
+                System.out.println("I regret to inform you that your substring does not exist in the string.");
+            } else {
+                System.out.println("Yay! Your substring exists in the string!");
+            }
         } else if (choice == 3) {
             System.out.println(stringLength);
         } else if (choice == 4) {
@@ -37,30 +44,28 @@ public class Main {
             String prefixInput = sc.nextLine();
             int lengthOfPrefixInput = prefixInput.length();
                 char prefix = prefixInput.charAt(lengthOfPrefixInput - 1);
-                //This takes the index of the input prefix and defines it.
                 int indexOfPrefix = startingString.indexOf(prefix);
-                //This defines "containsValue"
-                String containsValue = startingString.substring(indexOfPrefix);
-                String value = containsValue.substring(indexOfPrefix, stringLength - 1);
+                String value = startingString.substring(indexOfPrefix + 1, stringLength);
                 System.out.println(value);
-                //Make this do something
-                //int startOfValue = logMessage.indexOf("value=");
-                //String containsValue = logMessage.substring(startOfValue); //value=5;day=Tuesday;
-                //int equalsSignIndex = containsValue.indexOf('='); //5
-                //int semicolonIndex = containsValue.indexOf(';'); //7
-                //String value = containsValue.substring(equalsSignIndex + 1, semicolonIndex); //5
-                //between the fifth and seventh characters (= and ;), it gets the stuff between the two characters, which is the number 5.
-                //System.out.println(Integer.parseInt(value));
             }
-        } //else if (choice == 5) {
-           // System.out.println("Suffix:");
-           // String suffix = sc.nextLine();
+        else if (choice == 5) {
+           System.out.println("Suffix:");
+           String suffixInput = sc.nextLine();
+           int indexOfSuffix = startingString.indexOf(suffixInput);
+           String value = startingString.substring(0, indexOfSuffix);
+           System.out.println(value);
             //make this do something
-       // } else if (choice == 6) {
-         //   System.out.println("Prefix:");
-        //    String prefix = sc.nextLine();
-        //    System.out.println("Suffix:");
-        //    String suffix = sc.nextLine();
+       } else if (choice == 6) {
+         System.out.println("Prefix:");
+        String prefixInput = sc.nextLine();
+        System.out.println("Suffix:");
+        String suffixInput = sc.nextLine();
+        int indexOfSuffix = startingString.indexOf(suffixInput);
+            int lengthOfPrefixInput = prefixInput.length();
+            char prefix = prefixInput.charAt(lengthOfPrefixInput - 1);
+            int indexOfPrefix = startingString.indexOf(prefix);
+            String value = startingString.substring(indexOfPrefix + 1, indexOfSuffix);
+            System.out.println(value);
         }
-        //}
-  //  }
+        }
+}
